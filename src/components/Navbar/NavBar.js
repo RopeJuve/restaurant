8306ac501b0d000555b9ef526c26577f751b098b
Kiril <kiril_s@yahoo.com>
2022-05-31 13:15:28 +0200

    add german lenguage with /de

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -3,8 +3,10 @@ import Layout from "../Layout/Layout";
 import Image from "next/image";
 import logo from "../../assets/images/logo.png";
 import HeroSection from "../HeroSection/HeroSection";
+import { useTranslation } from "next-i18next";
 
-function NavBar() {
+const NavBar = () => {
+  const { t } = useTranslation();
   return (
     <Layout>
       <div className={styles.container}>
@@ -13,17 +15,17 @@ function NavBar() {
             <Image src={logo.src} alt="logo" width={150} height={150} />
           </div>
           <ul className={styles.menuContainer}>
-            <li>HOME</li>
-            <li>ABOUT US</li>
-            <li>ROOMS</li>
-            <li>RESERVATION</li>
-            <li>CONTACT</li>
+            <li>{`${t("navbar.home")}`}</li>
+            <li>{`${t("navbar.aboutUs")}`}</li>
+            <li>{`${t("navbar.rooms")}`}</li>
+            <li>{`${t("navbar.reservation")}`}</li>
+            <li>{`${t("navbar.contact")}`}</li>
           </ul>
         </div>
       </div>
       <HeroSection />
     </Layout>
   );
-}
+};
 
 export default NavBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import NavBar from "../components/Navbar/NavBar";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import nextI18nextConfig from "../../next-i18next.config";
 
- const Home = () => {
+const Home = () => {
   return (
     <div>
       <Head>
@@ -9,9 +11,20 @@ import NavBar from "../components/Navbar/NavBar";
         <meta name="description" content="Hotel" />
       </Head>
       <NavBar />
-     
     </div>
   );
 };
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(
+        locale || "de",
+        ["common"],
+        nextI18nextConfig
+      )),
+    },
+  };
+}
 
 export default Home;
